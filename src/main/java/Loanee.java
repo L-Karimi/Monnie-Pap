4e1c49ad424cbab68f24d359aec486cf8dbea675
EveNyarango <evenyarango@gmail.com>
2020-12-15 21:25:05 +0300

    create constants

--- src/main/java/Loanee.java
+++ src/main/java/Loanee.java
@@ -1,11 +1,16 @@
 public class Loanee {
-        private int id;
+    private int id;
     private String name;
     private int age;
     private String occupation;
     private int totalIncome;
     private int loanAmount;
     private String loanPurpose;
+
+
+    public static final String EMPLOYED = "employed";
+    public static final  String UNEMPLOYED = "unemployed";
+    public  static final String SELFEMPLOYED ="selfemployed"
 
 
     public Loanee(String name, int age, String occupation, int totalIncome, int loanAmount, String loanPurpose) {
@@ -15,5 +20,7 @@ public class Loanee {
         this.totalIncome = totalIncome;
         this.loanAmount = loanAmount;
         this.loanPurpose = loanPurpose;
+
+
     }
 }
