bbe2cb5c3c91a34e0984da5a41611affc5a5fc5e
EveNyarango <evenyarango@gmail.com>
2020-12-15 21:13:30 +0300

    add variables and constructors

--- src/main/java/Loanee.java
+++ src/main/java/Loanee.java
@@ -1,2 +1,19 @@
 public class Loanee {
+        private int id;
+    private String name;
+    private int age;
+    private String occupation;
+    private int totalIncome;
+    private int loanAmount;
+    private String loanPurpose;
+
+
+    public Loanee(String name, int age, String occupation, int totalIncome, int loanAmount, String loanPurpose) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.totalIncome = totalIncome;
+        this.loanAmount = loanAmount;
+        this.loanPurpose = loanPurpose;
+    }
 }
