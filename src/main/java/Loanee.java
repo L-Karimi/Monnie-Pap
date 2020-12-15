1539f940475f8058118ec85cb73ab88e1fd9a5bf
EveNyarango <evenyarango@gmail.com>
2020-12-15 21:28:54 +0300

    add getters and setters

--- src/main/java/Loanee.java
+++ src/main/java/Loanee.java
@@ -10,7 +10,7 @@ public class Loanee {
 
     public static final String EMPLOYED = "employed";
     public static final  String UNEMPLOYED = "unemployed";
-    public  static final String SELFEMPLOYED ="selfemployed"
+    public  static final String SELFEMPLOYED ="selfemployed";
 
 
     public Loanee(String name, int age, String occupation, int totalIncome, int loanAmount, String loanPurpose) {
@@ -21,6 +21,73 @@ public class Loanee {
         this.loanAmount = loanAmount;
         this.loanPurpose = loanPurpose;
 
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public int getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(int totalIncome) {
+        this.totalIncome = totalIncome;
+    }
+
+    public int getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(int loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public String getLoanPurpose() {
+        return loanPurpose;
+    }
+
+    public void setLoanPurpose(String loanPurpose) {
+        this.loanPurpose = loanPurpose;
+    }
+
+    public static String getEMPLOYED() {
+        return EMPLOYED;
+    }
+
+    public static String getUNEMPLOYED() {
+        return UNEMPLOYED;
+    }
+
+    public static String getSELFEMPLOYED() {
+        return SELFEMPLOYED;
     }
 }
