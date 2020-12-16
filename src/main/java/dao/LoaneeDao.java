c02829c74eebcbb62258e81dd55557b43a943886
EveNyarango <evenyarango@gmail.com>
2020-12-16 06:01:44 +0300

    add CRUD methods to the interface

--- src/main/java/dao/LoaneeDao.java
+++ src/main/java/dao/LoaneeDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Loanee;
+import org.graalvm.compiler.nodes.InliningLog;
+
+import java.util.List;
+
 public interface LoaneeDao {
+//    List
+    List<Loanee> getAllLoanee();
+
+//    Create
+    void add(Loanee loanee);
+
+//    Read
+    Loanee findById(int id);
+
+//    Update
+    void update(int id, String name, int age, String occupation, int totalIncome, int loanAmount, String loanPurpose);
+
+//     DELETE
+    void deleteById(int id);
+    void clearAllLoanee();
 }
