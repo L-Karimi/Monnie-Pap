475494e6107f2137032ec821c54823aecd1bea0d
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-16 22:03:31 +0300

    added new files

--- src/test/java/dao/sql2loanValidationDaoTest.java
+++ src/test/java/dao/sql2loanValidationDaoTest.java
@@ -1,9 +1,11 @@
+package dao;
+
 import org.junit.After;
 import org.junit.Before;
 
 import static org.junit.Assert.*;
 
-public class LoanValidationTest {
+public class sql2loanValidationDaoTest {
 
     @Before
     public void setUp() throws Exception {
