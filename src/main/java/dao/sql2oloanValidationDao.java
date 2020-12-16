77c8b50e0bf3709cd3a9ee81c8ed42c82f555e6f
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-16 22:52:52 +0300

    implement loanValidationDao

--- src/main/java/dao/sql2oloanValidationDao.java
+++ src/main/java/dao/sql2oloanValidationDao.java
@@ -1,4 +1,41 @@
 package dao;
 
-public class sql2oloanValidationDao {
+import models.Loanee;
+import org.sql2o.Sql2o;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class sql2oloanValidationDao implements loanValidationDao {
+    @Override
+    public List<loanValidationDao> getAllLoanValidationD() {
+        return null;
+    }
+
+    @Override
+    public void add(loanValidationDao loanValidationDao) {
+
+    }
+
+    @Override
+    public loanValidationDao findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, int age, String occupation, int totalIncome, int loanAmount, String loanPurpose) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAllLoanValidation() {
+
+    }
 }
