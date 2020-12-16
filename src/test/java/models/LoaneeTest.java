2ac7b8b60e9d9e354e1973f288ca5d26f4585fe5
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-16 12:34:15 +0300

    declare variables

--- src/test/java/models/LoaneeTest.java
+++ src/test/java/models/LoaneeTest.java
@@ -1,9 +1,11 @@
+package models;
+
 import org.junit.After;
 import org.junit.Before;
 
 import static org.junit.Assert.*;
 
-public class LoanValidationTest {
+public class LoaneeTest {
 
     @Before
     public void setUp() throws Exception {
