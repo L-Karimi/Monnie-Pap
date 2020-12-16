4a167205e2ba66529de8c880e4b15501c2bcf500
Francisndungu2 <francianmbugua@gmail.com>
2020-12-16 12:58:45 +0300

    addsql

---
+++ create.sql
@@ -0,0 +1,14 @@
+SET MODE PostgreSQL;
+
+CREATE DATABASE lend;
+
+    \c lend
+
+    CREATE TABLE IF NOT EXISTS lend (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    age int,
+    occupation VARCHAR,
+    totalincome int,
+    loanamount int,
+    loanpurpose VARCHAR);
