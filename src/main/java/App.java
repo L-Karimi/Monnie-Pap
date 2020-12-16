851c6b07c9ab80db6de458f13da2fcb319c69e1a
Mariga123 <johnmariga8@gmail.com>
2020-12-17 01:17:07 +0300

    landing page workspace

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+//import spark.ModelAndView;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class App {
+//
+//    get("/", (request, response) -> {
+//        Map<String, Object> model = new HashMap<>();
+//        return new ModelAndView(model, "index.hbs");
+//    }, new
+//}
