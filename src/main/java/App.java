0ec8f855e9aff07d94f131381c36ccbbf384bd3b
EveNyarango <evenyarango@gmail.com>
2020-12-17 10:34:45 +0300

    create homepage routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,25 @@
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    Sql2o sql2o = new Sql2o ("jdbc:postgresql://localhost:5432/lend", "moringa", "Nya2rango@");
+
+    public static void main (String[] args) {
+        staticFileLocation("/public");
+
+//        homepage
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
 
 }
